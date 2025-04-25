bb29aad01ae6a017fc39aa79c461c250130dbe0b
Rodolfo <elproferodofer@gmail.com>
2025-04-25 11:41:14 -0400

    up

--- 2. Decisiones/3. Numero mayor.py
+++ 2. Decisiones/3. Numero mayor.py
@@ -7,9 +7,12 @@ numero1 = int(input("Ingresa primer numero: "))
 numero2 = int(input("Ingresa segundo numero: "))
 
 if numero1 > numero2:
-    print(f"{numero1} es mayor que {numero2}")
+    #print(f"{numero1} es mayor que {numero2}")
+    print(numero1, "es mayor que", numero2)
 else:
     if numero2 > numero1:
-        print(f"{numero2} es mayor que {numero1}")
+        #print(f"{numero2} es mayor que {numero1}")
+        print(numero2, "es mayor que", numero1)
     else:
-        print(f"{numero1} es igual a {numero2}")
+        #print(f"{numero1} es igual a {numero2}")
+        print(numero1, "es igual a", numero2)
