830d0e6556727a99b5a0c7c8a5610137f89fc51d
Dan1elCastroC <danie.castroc@duocuc.cl>
2025-06-11 13:57:27 -0400

    Update Daniel.Castro.Funsion.def.py

--- Daniel.Castro.Funsion.def.py
+++ Daniel.Castro.Funsion.def.py
@@ -1,9 +1,10 @@
 def sumar_quince_numeros():
-    suma = 0
-    contador = 1
-    while contador <= 15:
-        numero = input("Ingrese un número: ")
-        suma = suma + int(numero)
-        contador = contador + 1
-    print("La suma de los 15 números es:", suma)
-sumar_quince_numeros()
+    suma= 0
+    contador= 1
+    while contador<= 15:
+        numero= input("Ingrese un número: ")
+        suma= suma+ int(numero)
+        contador+= 1
+    return suma
+
+print("La suma de 15 números:", sumar_quince_numeros())
