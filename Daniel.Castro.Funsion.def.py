3a66706e3e632760d48a3b30417f15a2e2e000db
Dan1elCastroC <danie.castroc@duocuc.cl>
2025-06-11 14:11:39 -0400

    Update Daniel.Castro.Funsion.def.py

--- Daniel.Castro.Funsion.def.py
+++ Daniel.Castro.Funsion.def.py
@@ -1,3 +1,4 @@
+# 1. Sumar 15 números ingresados
 def sumar_quince_numeros():
     suma= 0
     contador= 1
@@ -8,3 +9,14 @@ def sumar_quince_numeros():
     return suma
 
 print("La suma de 15 números:", sumar_quince_numeros())
+
+# 2. Verificar mayoría de edad
+def verificar_mayoria_edad():
+    edad= input("Ingrese su edad: ")
+    edad= int(edad)
+    if edad>= 18:
+        return "Es mayor de edad."
+    else:
+        return "Es menor de edad."
+
+print("Verificación de edad:", verificar_mayoria_edad())
